2c8e2018eb0a18f6decdd04d0f1f337361696d4c
Rathan-07 <rathankudukulli07@gmail.com>
2024-06-13 10:29:26 +0530

    first-commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const {connect} = mongoose
 const ConfigureDB = async ()=>{
     try{
-        const db =  await connect('mongodb://127.0.0.1:27017/blog-app')
+        // const db =  await connect('mongodb://127.0.0.1:27017/blog-app')
+        const db = await mongoose.connect('mongodb+srv://rathan:rathan%4007@cluster0.jhevnjk.mongodb.net/blog-app?retryWrites=true&w=majority&appName=Cluster0');
         console.log('connected to db');
     }
     catch(err){
