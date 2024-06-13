efcd1629105f20b108d3483b68703236ac3207ae
Rathan-07 <rathankudukulli07@gmail.com>
2024-06-13 11:26:52 +0530

    db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const ConfigureDB = async ()=>{
     try{
         // const db =  await connect('mongodb://127.0.0.1:27017/blog-app')
         const db = await mongoose.connect(process.env.MONGO_URL);
-        console.log('connected to db');
+        console.log('connected to database');
     }
     catch(err){
         console.log(err);
